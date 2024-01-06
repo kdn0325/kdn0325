9b3a23035fc4c0431a73c5da3d9b4478956a3892
Abel <dn10003@gmail.com>
2024-01-06 21:23:33 +0900

    post-update

--- post.py
+++ post.py
@@ -1,4 +1,6 @@
 import feedparser
+from datetime import datetime
+from time import mktime
 
 blog_url = "https://kdn0325.github.io/feed.xml"
 rss_feed = feedparser.parse(blog_url)
@@ -7,11 +9,16 @@ MAX_NUM = 3
 
 latest_posts = ""
 
-for idx, entrie in enumerate(rss_feed['entries']):
-  if idx > MAX_NUM:
-     break;
-  feed_date = entrie['published_parsed']
-  latest_posts += f" - [{feed_date.tm_mon}/{feed_date.tm_mday} - {entrie['title']}]({entrie['link']})\n"
+for idx, entry in enumerate(rss_feed['entries']):
+    if idx >= MAX_NUM:
+        break
+
+    feed_date = entry['published_parsed']
+    formatted_date = datetime.fromtimestamp(
+        mktime(feed_date)
+    ).strftime('%Y년 %m월 %d일')
+
+    latest_posts += f" - [📆{formatted_date} /{entry['title']}]({entry['link']})\n"
 
 print(latest_posts)
 
@@ -36,38 +43,38 @@ preREADME = """
 <img src="https://img.shields.io/badge/react-61DAFB.svg?style=for-the-badge&logo=react&logoColor=white" />
 <img src="https://img.shields.io/badge/nextjs-000000.svg?style=for-the-badge&logo=nextdotjs&logoColor=white" />
 </p>
- 
+
  #### Mobile Applications
  <p align="left">
 <img src="https://img.shields.io/badge/reactnative-282c35.svg?style=for-the-badge&logo=react&logoColor=white" />
-  
+
  #### State management
  <p align="left">
 <img src="https://img.shields.io/badge/redux-764ABC.svg?style=for-the-badge&logo=redux&logoColor=white" />
 <img src="https://img.shields.io/badge/recoil-3578E5.svg?style=for-the-badge&logo=recoil&logoColor=white" />
 <img src="https://img.shields.io/badge/reactquery-FF4154.svg?style=for-the-badge&logo=reactquery&logoColor=white" />
-  
+
  #### CSS
  <p align="left">
 <img src="https://img.shields.io/badge/styledcomponents-DB7093.svg?style=for-the-badge&logo=styledcomponents&logoColor=white" />
 <img src="https://img.shields.io/badge/sass-CC6699.svg?style=for-the-badge&logo=sass&logoColor=white" />
 <img src="https://img.shields.io/badge/tailwindcss-06B6D4.svg?style=for-the-badge&logo=tailwindcss&logoColor=white" />
- 
- 
+
+
  #### Backend
  <p align="left">
 <img src="https://img.shields.io/badge/nodejs-339933.svg?style=for-the-badge&logo=nodedotjs&logoColor=white" />
 <img src="https://img.shields.io/badge/django-092E20.svg?style=for-the-badge&logo=django&logoColor=white" />
 <img src="https://img.shields.io/badge/firebase-FFCA28.svg?style=for-the-badge&logo=firebase&logoColor=white" />
- 
- 
+
+
  #### DevOps
  <p align="left">
 <img src="https://img.shields.io/badge/mysql-4479A1.svg?style=for-the-badge&logo=mysql&logoColor=white" />
 <img src="https://img.shields.io/badge/mongodb-47A248.svg?style=for-the-badge&logo=mongodb&logoColor=white" />
 <img src="https://img.shields.io/badge/AWS-232F3E.svg?style=for-the-badge&logo=amazon-aws&logoColor=white"  />
- 
- 
+
+
  #### Build Tools
 <p align="left">
 <img src="https://img.shields.io/badge/eslint-4B32C3.svg?style=for-the-badge&logo=eslint&logoColor=white" />
