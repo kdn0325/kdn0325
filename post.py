d940206556639c046f05c4d06bf22095839018dd
DONGYEONG <91298955+kdn0325@users.noreply.github.com>
2024-07-30 00:59:25 +0900

    Update post.py

--- post.py
+++ post.py
@@ -25,11 +25,3 @@ print(latest_posts)
 preREADME = """
 
 <img src="https://raw.githubusercontent.com/kdn0325/terminal-for-github-profile-readme/1adccd811a108350e5dbe91c5e4911c04bd6f289/github_stats.svg" />
-
-## 최근 포스트
-"""
-
-resultREADME = f"{preREADME}{latest_posts}"
-
-with open("README.md", "w", encoding='utf-8') as f :
-  f.write(resultREADME)
