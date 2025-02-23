23a86b9be4cf7b9053031aadb656be30b16b0afe
DONGYEONG <91298955+kdn0325@users.noreply.github.com>
2025-02-23 21:08:18 +0900

    Update post.py

--- post.py
+++ post.py
@@ -1,23 +1,27 @@
-import os
+import feedparser
 
-def update_readme():
-    readme_path = 'README.md'
-    image_url = 'https://raw.githubusercontent.com/kdn0325/terminal-for-github-profile-readme/1adccd811a108350e5dbe91c5e4911c04bd6f289/github_stats.svg'
-    
-    # 읽어온 README 파일 내용
-    with open(readme_path, 'r') as file:
-        content = file.readlines()
-    
-    # 이미지 URL 업데이트
-    new_content = []
-    for line in content:
-        if '<img src="' in line:
-            line = f'<img src="{image_url}" />\n'
-        new_content.append(line)
-    
-    # 수정된 내용으로 README 파일 업데이트
-    with open(readme_path, 'w') as file:
-        file.writelines(new_content)
+blog_url = "https://kdn0325.github.io/feed.xml"
+rss_feed = feedparser.parse(blog_url)
 
-if __name__ == "__main__":
-    update_readme()
+MAX_NUM = 6
+
+latest_posts = ""
+
+for idx, entrie in enumerate(rss_feed['entries']):
+  if idx > MAX_NUM:
+     break;
+  feed_date = entrie['published_parsed']
+  latest_posts += f" - [{feed_date.tm_mon}/{feed_date.tm_mday} - {entrie['title']}]({entrie['link']})\n"
+
+print(latest_posts)
+
+preREADME = """
+
+
+## 최근 포스트
+"""
+
+resultREADME = f"{preREADME}{latest_posts}"
+
+with open("README.md", "w", encoding='utf-8') as f :
+  f.write(resultREADME)
